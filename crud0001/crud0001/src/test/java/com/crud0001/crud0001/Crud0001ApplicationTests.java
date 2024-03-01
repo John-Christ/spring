cb592c4411e0aa7af2e8e043939ba83805a09f6f
john christ bouma <52007598+John-Christ@users.noreply.github.com>
2024-03-02 00:47:48 +0200

    Add files via upload

---
+++ crud0001/crud0001/src/test/java/com/crud0001/crud0001/Crud0001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud0001.crud0001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crud0001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ crud21/crud21/src/main/java/com/crud21/crud21/Crud21Application.java
@@ -0,0 +1,13 @@
+package com.crud21.crud21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crud21Application.class, args);
+	}
+
+}
